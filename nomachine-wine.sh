c9fd87212836bb82f1d42125057fdb872d861983
top4u <53888397+top4u@users.noreply.github.com>
2025-07-11 22:24:27 -0700

    Update nomachine-wine.sh

--- nomachine-wine.sh
+++ nomachine-wine.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://github.com/top4u/hashirgcl1/raw/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
@@ -20,7 +20,7 @@ read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
 
 clear
-echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
+echo "Repo: https://github.com/top4u/hashirgcl1"
 echo "======================="
 echo "choose ngrok region (for better connection)."
 echo "======================="
